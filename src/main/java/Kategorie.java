8052271f3954e8ff3793a57300c8b16a9842f357
Marlena Kurczab <mojca13@gazeta.pl>
2025-07-31 15:34:57 +0200

    opisane wszystkie kategorie dostępne w Polsce.

--- src/main/java/Kategorie.java
+++ src/main/java/Kategorie.java
@@ -10,8 +10,16 @@ public class Kategorie {
         String[] katA = {"A","20 lat/24 lata","motocykl+pojazdy z kat. AM","do 24 lat:2 lata z kat.A2,24 lata i starsi:brak"};
         String[] katB1 = {"B1","16 lat","minisamochód,wszystkie quady i pojazdy z kat.AM","przed 18:zgoda rodziców,starsi-brak wymagań,choć lepiej celować w kat. B"};
         String[] katB = {"B","18 lat","samochód osobowy+pojazdy z kat.B1","brak"};
-        String[] katBE = {"B+E","18 lat","samochód osobowy z przyczepą","posiadanie prawa jazdy kategorii B"};
-
+        String[] katBE = {"B+E","18 lat","samochód osobowy z przyczepą+pojazdy z kat.B","posiadanie prawa jazdy kategorii B"};
+        String[] katC1 = {"C1","18 lat","ciężarówka do 7,5 tony,zespół złożony z tego pojazdu i przyczepy lekkiej+pojazdy z kat.AM","posiadanie prawa jazdy kategorii B"};
+        String[] katC1E = {"C1+E","18 lat","ciężarówka z przyczepą-łączna masa zestawu max 12 ton i pojazdy z kat. C1","posiadanie prawa jazdy kategorii C1"};
+        String[] katC = {"C","18 lat/21 lat","ciężarówka,zespół złożony z tego pojazdu i przyczepy lekkiej+pojazdy z kat. AM","posiadanie prawa jazdy kategorii B,do 21 lat kwalifikacja wstępna"};
+        String[] katCE = {"C+E","18 lat/21 lat","ciężarówka z przyczepą+pojazdy z kat. C","posiadanie prawa jazdy kategorii C,do 21 lat kwalifikacja wstępna"};
+        String[] katD1 = {"D1","21 lat","autobus do 16 pasażerów,zespół złożony z tego pojazdu i przyczepy lekkiej+pojazdy z kat. AM","posiadanie prawa jazdy kategorii B"};
+        String[] katD1E = {"D1+E","21 lat","autobus do 16 pasażerów z przyczepą+pojazdy z kat.D1","posiadanie prawa jazdy kategorii D1"};
+        String[] katD = {"D","21 lat/23 lata/24 lata","autobus,zestaw złożony z tego pojazdu i przyczepy lekkiej+pojazdy z kat. AM","posiadanie prawa jazdy kategorii B,do 23 lat kwalifikacja wstępna,23 do 24 lat:kwalifikacja wstępna przyspieszona"};
+        String[] katDE = {"D+E","21 lat/23 lata/24 lata","autobus z przyczepą+pojazdy z kat.D","posiadanie prawa jazdy kategorii D,do 23 lat kwalifikacja wstępna,23 do 24 lat:kwalifikacja wstępna przyspieszona"};
+        String[] katT = {"T","16 lat","ciągnik lub pojazd wolnobieżny-z przyczepą lub bez+pojazdy z kat. AM","przed 18:zgoda rodziców,starsi-brak wymagań"};
         System.out.println(Arrays.toString(kategoria));
         System.out.println(Arrays.toString(katAM));
         System.out.println(Arrays.toString(katA1));
@@ -20,5 +28,14 @@ public class Kategorie {
         System.out.println(Arrays.toString(katB1));
         System.out.println(Arrays.toString(katB));
         System.out.println(Arrays.toString(katBE));
+        System.out.println(Arrays.toString(katC1));
+        System.out.println(Arrays.toString(katC1E));
+        System.out.println(Arrays.toString(katC));
+        System.out.println(Arrays.toString(katCE));
+        System.out.println(Arrays.toString(katD1));
+        System.out.println(Arrays.toString(katD1E));
+        System.out.println(Arrays.toString(katD));
+        System.out.println(Arrays.toString(katDE));
+        System.out.println(Arrays.toString(katT));
     }
 }
