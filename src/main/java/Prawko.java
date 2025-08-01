0cac744fb371cd93a4fdc6b4e48baaee3abac9af
Marlena Kurczab <mojca13@gazeta.pl>
2025-08-01 23:18:07 +0200

    dodana klasa-zadania z egzaminu praktycznego.

--- src/main/java/Prawko.java
+++ src/main/java/Prawko.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
+
 public class Prawko {
     public static void main(String[] args) {
         InformacjaOKategoriach informacjaOKategoriach = new InformacjaOKategoriach();
+        EgzaminZJazdy egzaminZJazdy = new EgzaminZJazdy();
+        Scanner autko = new Scanner(System.in);
+
         informacjaOKategoriach.Wyswietl();
     }
 }
