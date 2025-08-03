bfa0d3037fddad84f0ee5421d10bdf3fdd5e8521
Marlena Kurczab <mojca13@gazeta.pl>
2025-08-03 18:57:58 +0200

    informacje,cz.3

--- src/main/java/EgzaminZJazdy.java
+++ src/main/java/EgzaminZJazdy.java
@@ -4,9 +4,9 @@ public class EgzaminZJazdy {
         System.out.println("Po zdanym egzaminie teoretycznym dla danej kategorii,można podejść do części praktycznej;jeśli nie przewidziano części teoretycznej,można zdawać egzamin z jazdy od razu.");
     }
     public void PrzejedzPlac() {
-
+        System.out.println("Jazda po placu manewrowym.");
     }
     public void PrzejedzMiasto() {
-
+        System.out.println("Jazda po mieście.");
     }
 }
